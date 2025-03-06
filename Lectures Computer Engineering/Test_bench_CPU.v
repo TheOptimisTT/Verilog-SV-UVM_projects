7ee61dbe6590f11d8d93315829f974d48db5cccc
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2025-03-06 22:07:59 +0200

    CPU update
    Changed the stimulus and meminit file back to txt. Fixed the Testbench

--- Lectures Computer Engineering/Test_bench_CPU.v
+++ Lectures Computer Engineering/Test_bench_CPU.v
@@ -5,15 +5,17 @@ module Test_Bench_LU();
 	reg[7:0] ICODE;
 	reg[7:0] dsgn_stm[0:15];
 	reg[3:0] addr_index;
-    integer i;
-    integer ii;
+    integer i, ii;
 	
-	lu_processor lu_proc(.clk(clk),
-		     	     .rst(rst),
-		     	     .ICODE(ICODE));
+	lu_processor lu_proc(
+					.clk(clk),
+		     	    .rst(rst),
+                    .ICODE(ICODE)
+						);
 
 	initial begin
 		clk = 0;
+      ICODE = 15;
 		forever begin
 			#5;
 			clk = ~clk;
@@ -21,33 +23,26 @@ module Test_Bench_LU();
 	end
 	
 	initial begin
-		//$readmemb("stimulus.psj", dsgn_stm, 0);
-		//$readmemb("mem_init.psj", lu_proc.SyncMEM2P_instance1.mem, 0);
-        for(i = 0; i<16; i=i+1)begin
-            dsgn_stm[i] = i;
-        end
-        for(ii = 0; ii<256; ii=ii+1)begin
-            $display("ii=%0d",ii);
-            lu_proc.SyncMEM2P_instance1.mem[ii] = 16'b0;
-        end
-		#1000;
+		$readmemb("stimulus.txt", dsgn_stm, 0);
+		$readmemb("mem_init.txt", lu_proc.SyncMEM2P_instance1.mem, 0);
+		$display(dsgn_stm);
+		$display(lu_proc.SyncMEM2P_instance1.mem);
 		$finish();
 	end
 	
 	initial begin
-        $dumpfile("wave.vcd"); // Name of VCD file
-        $dumpvars(3, Test_Bench_LU);    // Dump all signals in 'test' module
+        $dumpfile("wave.vcd"); 
+        $dumpvars(3, Test_Bench_LU);
 		addr_index = 0;
 		rst = 1;//CPU in reset
+		ICODE = 0;
 		#12;
 		rst = 0; //CPU active
-		ICODE = 0;
+      	#10;
 		forever begin
 			@(posedge clk);
 			ICODE = dsgn_stm[addr_index];
 			addr_index = addr_index+1;
 		end
 	end
-
-	
 endmodule
